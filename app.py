8165b6a219da670ce57658dc2532e97a597f6441
Denis <58739351+davidov-denis@users.noreply.github.com>
2021-04-05 19:20:32 +0300

    aaaaa

--- app.py
+++ app.py
@@ -158,6 +158,6 @@ def contact():
 # UPDATE orders SET iscomplet=true WHERE id=5; ОБНОВЛЕНИЕ БАЗЫ ДАННЫХ
 # SELECT * FROM orders;
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
